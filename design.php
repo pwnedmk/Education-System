73baadc1478d6fee6438abb9afc0b97d72d37a61
SuengJunJung <99368760+SuengJunJung@users.noreply.github.com>
2024-03-12 08:07:03 -0500

    Add files via upload
    the php file and html file are same. js is for calendar

---
+++ design.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+    <script src="javascript.js"></script>
+    <link rel="stylesheet" type="text/css" href="test.css">
+</head>
+<body>
+    <nav id="menu_area">
+        <ul>
+            <li>Student List</li>
+            <li>Create Assignment</li>
+            <li>Grade Assignment</li>
+        </ul>
+    </nav>
+    <div id="content_area">
+        <div id="list">
+            <ul>
+                <li>.</li>
+                <li>student list with assignment</li>
+                <li></li>
+            </ul>
+        </div>
+        <hr>
+        <div id="horizontal_section">
+            <div class="col" id="top_performer">
+                <ul>
+                    <li>.</li>
+                    <li>top performer</li>
+                    <li>.</li>
+                </ul>
+            </div>
+            <div class="col2" id="none">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col3" id="calendar">
+                <div class="sec_cal">
+                    <div class="cal_nav">
+                        <a href="javascript:;" class="nav-btn go-prev">prev</a>
+                        <div class="year-month"></div>
+                        <a href="javascript:;" class="nav-btn go-next">next</a>
+                    </div>
+                    <div class="cal_wrap">
+                        <div class="days">
+                            <div class="day">MON</div>
+                            <div class="day">TUE</div>
+                            <div class="day">WED</div>
+                            <div class="day">THU</div>
+                            <div class="day">FRI</div>
+                            <div class="day">SAT</div>
+                            <div class="day">SUN</div>
+                        </div>
+                        <div class="dates"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ javascript.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+    calendarInit();
+});
+function calendarInit() {
+    var usaTimezone = 'America/Chicago'; 
+    var today = new Date(new Date().toLocaleString('en-US', { timeZone: usaTimezone }));
+
+    // Set the reference time for the calendar
+    var thisMonth = today;
+
+    // converted time (thisMonth) and today
+    var currentYear = thisMonth.getFullYear();
+    var currentMonth = thisMonth.getMonth();
+    var currentDate = today.getDate();
+
+    // Calendar rendering
+    renderCalender(thisMonth);
+
+    function renderCalender(thisMonth) {
+
+        // Organizing Data for Rendering
+        currentYear = thisMonth.getFullYear();
+        currentMonth = thisMonth.getMonth();
+        currentDate = thisMonth.getDate();
+
+        // Find the date and day of the last day of the previous month
+        var startDay = new Date(currentYear, currentMonth, 0);
+        var prevDate = startDay.getDate();
+        var prevDay = startDay.getDay();
+
+        // Find the date and day of the month on the last day of the month
+        var endDay = new Date(currentYear, currentMonth + 1, 0);
+        var nextDate = endDay.getDate();
+        var nextDay = endDay.getDay();
+
+        // console.log(prevDate, prevDay, nextDate, nextDay);
+
+        // Current month notation
+        document.querySelector('.year-month').textContent = currentYear + '.' + (currentMonth + 1);
+
+        // Creating a rendering html element
+        var calendar = document.querySelector('.dates');
+                calendar.innerHTML = '';
+        
+                // previeous month
+                for (var i = prevDate - prevDay + 1; i <= prevDate; i++) {
+                    calendar.innerHTML += '<div class="day prev disable">' + i + '</div>';
+                }
+                // this month
+                for (var i = 1; i <= nextDate; i++) {
+                    calendar.innerHTML += '<div class="day current">' + i + '</div>';
+                }
+                // next month
+                for (var i = 1; i <= (7 - nextDay == 7 ? 0 : 7 - nextDay); i++) {
+                    calendar.innerHTML += '<div class="day next disable">' + i + '</div>';
+                }
+
+        // Indicate today's date
+        if (today.getMonth() == currentMonth) {
+            var currentMonthDate = document.querySelectorAll('.dates .current');
+            currentMonthDate[currentDate - 1].classList.add('today');
+        }
+    
+    }
+
+    // go prev
+    document.querySelector('.go-prev').addEventListener('click', function() {
+        thisMonth = new Date(currentYear, currentMonth - 1, 1);
+        renderCalendar(thisMonth);
+    });
+
+    // go next
+    document.querySelector('.go-next').addEventListener('click', function() {
+        thisMonth = new Date(currentYear, currentMonth + 1, 1);
+        renderCalendar(thisMonth); 
+    });
+}
