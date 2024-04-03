ec6b6b7249fbbd2244543698a1f6f25610af2cfb
nicholaswinona <148025524+nicholaswinona@users.noreply.github.com>
2024-04-03 08:23:32 -0500

    Change gitignore // database
    Realized we should try to keep the same database. After making big changes/making tables, try to add the change to the educationsystem.sql file by exporting your current database and renaming it educatoinsystem.sql if possible

---
+++ educationsystem.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2024 at 03:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `educationsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `userID` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `userID`, `password`, `type`, `name`, `email`) VALUES
+(4, 'Jun', '1234', 'student', 'jun1', 'sjung1359@gmail.com'),
+(6, 'jun34', '1234', 'student', 'Jung', 'sjung1359@gmail.com'),
+(7, 'ki', '1234', 'student', 'kiki', 'sjung1359@gmail.com'),
+(8, 'bo', '1234', 'student', 'bong', 'asd@asd.com'),
+(9, '1234', '1234', 'teacher', '1234', '1234@1234.com'),
+(10, 'qwer', '1234', 'student', 'Jun', '1234@1234.com'),
+(11, 'Jun123', '1234', 'teacher', 'teacher', 'sj@asd.com'),
+(12, 'Jun1234', '1234', 'student', '1234', '1234@1234.com'),
+(13, '9999', '9999', 'student', '9999', '999@999.com'),
+(14, 'ndever', '1234', 'student', 'Nicholas Dever', 'nd@email.com'),
+(19, 'ndever34', 'Password1234!', 'student', 'Nicholas', 'nd@gmail.com'),
+(20, 'testUser', 'pass', 'student', 'Test', 'Test@email.com'),
+(21, 'ndever01', 'abc123!lA', 'student', 'Nicholas', 'nd@gmails.com'),
+(22, 'ndevT', 'Password123!', 'teacher', 'Teach Nick', 'teacher@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_submissions`
+--
+
+CREATE TABLE `student_submissions` (
+  `id` int(11) NOT NULL,
+  `assignment_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_submissions`
+--
+
+INSERT INTO `student_submissions` (`id`, `assignment_id`, `student_id`, `file_path`, `submitted_at`) VALUES
+(0, 0, 0, 'upload_stu/studentupload.txt', '2024-04-03 07:38:21'),
+(0, 0, 0, 'upload_stu/studentupload.txt', '2024-04-03 07:42:48'),
+(0, 0, 1, 'upload_stu/studentupload.txt', '2024-04-03 07:49:14'),
+(0, 0, 4, 'upload_stu/studentupload.txt', '2024-04-03 07:53:11'),
+(0, 0, 4, 'upload_stu/studentupload.txt', '2024-04-03 07:53:23'),
+(0, 0, 14, 'upload_stu/studentupload.txt', '2024-04-03 08:02:22'),
+(0, 0, 1, 'upload_stu/studentupload.txt', '2024-04-03 12:46:52'),
+(0, 1, 21, 'upload_stu/studentupload23.txt', '2024-04-03 12:51:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_assignments`
+--
+
+CREATE TABLE `teacher_assignments` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `file_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher_assignments`
+--
+
+INSERT INTO `teacher_assignments` (`id`, `title`, `description`, `file_path`) VALUES
+(1, 'sads', 'fsfds', 'uploads/login.css'),
+(2, '123123', '123123', 'uploads/login.css'),
+(3, '15555', '555555', 'uploads/student_page.php'),
+(4, 'New assignment', 'asdfwer123', 'uploads/teacheruplad.txt');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`userID`);
+
+--
+-- Indexes for table `teacher_assignments`
+--
+ALTER TABLE `teacher_assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `teacher_assignments`
+--
+ALTER TABLE `teacher_assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
