dc13ee694f9528d54a28e190ab4dedd0de1ace5a
SuengJunJung <sjung1359@gmail.com>
2024-04-24 20:43:07 -0500

    add nav

--- createNewuser.php
+++ createNewuser.php
@@ -8,7 +8,15 @@
     
 </head>
 <body>
-
+<nav id="menu_area">
+    <ul>
+        <li><a href="admin_page.php">Home</a></li>
+        <li><a href="studentlist1.php">Student List</a></li>
+        <li><a href="teacherlist.php">Teacher List</a></li>
+        <li><a href="createNewuser.php">Create New User</a></li>
+        <li><a href="logout.php"><button>Logout</button></a></li>
+    </ul>
+</nav>
 
 
 <!-- New User Form -->
